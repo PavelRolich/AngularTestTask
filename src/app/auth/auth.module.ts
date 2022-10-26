697ef70a2e4f56f0cfaf1d389f77defe1f7a2207
PavelRolich <pavelrolich@gmail.com>
2022-10-26 21:39:01 +0300

    updated import material module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { MaterialModule } from '../core/modules/material.module';
 
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ButtonModule } from '../core/components/button/button.module';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent],
-  imports: [CommonModule, AuthRoutingModule, MaterialModule, FormsModule, ReactiveFormsModule, ButtonModule],
+  imports: [CommonModule, AuthRoutingModule, MatCardModule, FormsModule, ReactiveFormsModule, ButtonModule],
 })
 export class AuthModule {}
