a058811317dfc951261201f22b8ea85e470b9a92
PavelRolich <pavelrolich@gmail.com>
2022-10-26 19:15:00 +0300

    import modules to root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarModule } from './core/components/navbar/navbar.module';
+import { SnackbarModule } from './core/components/snackbar/snackbar.module';
+
+import { httpInterceptorProviders } from './core/interceptors';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { StoreModule } from '@ngrx/store';
+import { authReducer } from './core/store/reducers/auth.reducer';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
-  providers: [],
+  imports: [
+    NavbarModule,
+    SnackbarModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({ auth: authReducer }, {}),
+  ],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
