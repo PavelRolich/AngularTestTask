0faa47d3fd59333a2951677c2259a6d4b576a898
PavelRolich <pavelrolich@gmail.com>
2022-10-27 11:24:34 +0300

    updated review module

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from '../core/store/state';
+import { Observable, of } from 'rxjs';
 import { Product } from '../core/interfaces/product.interface';
-import { ReviewForm } from '../core/interfaces/review.interface';
+import { Review, ReviewForm } from '../core/interfaces/review.interface';
 import { ReviewService } from '../core/services/review.service';
+import { selectReviews } from '../core/store/selectors/reviews.selectors';
 
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.scss'],
 })
-export class ReviewComponent {
+export class ReviewComponent implements OnInit {
   @Input() activeProduct!: Product;
 
-  constructor(private reviewService: ReviewService) {}
+  reviewList$: Observable<Review[]> = of([]);
+
+  constructor(private reviewService: ReviewService, private store: Store<State>) {
+    this.reviewList$ = this.store.select(selectReviews);
+  }
+
+  ngOnInit(): void {
+    this.reviewService.getReviewList(this.activeProduct.id);
+  }
 
   submitReviewForm(form: ReviewForm): void {
     this.reviewService.sendReview(form.text, form.rate, this.activeProduct.id);
--- src/app/review/review.module.ts
+++ src/app/review/review.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { StarModule } from '../core/components/star/star.module';
+import { ButtonModule } from '../core/components/button/button.module';
+
+import { ReviewService } from '../core/services/review.service';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
 import { ReviewComponent } from './review.component';
 import { ReviewFormComponent } from './review-form/review-form.component';
 import { ReviewListComponent } from './review-list/review-list.component';
-import { StarModule } from '../core/components/star/star.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { ButtonModule } from '../core/components/button/button.module';
-import { ReviewService } from '../core/services/review.service';
+import { ReviewListItemComponent } from './review-list-item/review-list-item.component';
 
 @NgModule({
-  declarations: [ReviewComponent, ReviewFormComponent, ReviewListComponent],
-  imports: [CommonModule, StarModule, MatFormFieldModule, MatInputModule, FormsModule, ButtonModule],
+  declarations: [ReviewComponent, ReviewFormComponent, ReviewListComponent, ReviewListItemComponent],
+  imports: [CommonModule, StarModule, MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, ButtonModule],
   exports: [ReviewComponent],
   providers: [{ provide: ReviewService, useClass: ReviewService }],
 })
